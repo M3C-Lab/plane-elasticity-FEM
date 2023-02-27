26f5eb1f74f652c1ee9db952b277df062e7a96bc
Xuanming Huang <112839307+Goldeninscriptions@users.noreply.github.com>
2023-02-27 20:02:08 +0800

    Quadrature rules for triangular parent element
    Notes of parent element:
    p1(-1, -1)
    p2(1, -1)
    p3(0, 1)
    on Page 121

---
+++ TriangularQuad.m
@@ -0,0 +1,192 @@
+function [qp, wq, nqp] = TriangularQuad(degree)
+% To generate Hammer's quadrature info on Page 173 for the default 
+% parent triangle on Page 121 with parent coordinates (xi, eta).
+% Input:
+%   degree: The degree of precision.
+% Output:
+%   qp: The coodinates in the parameter space (xi, eta).
+%   wq: The quadrature weight.
+%   nqp: The number of quadrature points.
+
+% !!!!!! Note: We will make the number of quadrature points (nqp) as little 
+%   as possible. The available groups are:
+%   (nqp, degree) = 
+%   (3, 2)  (4, 3)  (6, 4)  (7, 5)  (12, 6) (13, 7)
+% !!!!!! Note:  The triangular coordinates (r, s, t) would be mapped into
+%   parameter coodinates(xi, eta)
+% The nodes of the default parent triangle:
+% p1:(-1, -1)   p2:(1, -1)  p3:(0, 1)
+
+if degree == 2
+    nqp = 3;
+    triangular_qp = zeros(3, nqp); % qp with triangular coordinates
+    wq = zeros(1, nqp);
+    for ii = 1 : 3
+        for jj = 1 : 3
+            if ii == jj
+                triangular_qp(ii, jj) = 0.666666666666667;
+            else
+                triangular_qp(ii, jj) = 0.166666666666667;
+            end
+            wq(1, jj) = 0.333333333333333;
+        end
+    end
+    
+elseif degree == 3
+    nqp = 4;
+    triangular_qp = zeros(3, nqp);
+    wq = zeros(1, nqp);
+    for ii = 1 : 3
+        triangular_qp(ii, 1) = 0.333333333333333;
+        for jj = 2 : 4
+            if ii == jj - 1
+                triangular_qp(ii, jj) = 0.6;
+            else
+                triangular_qp(ii, jj) = 0.2;
+            end
+            wq(1, jj) = 0.520833333333333;
+        end
+    end
+    wq(1, 1) = -0.56250;
+
+elseif degree == 4
+    nqp = 6;
+    triangular_qp = zeros(3, nqp);
+    wq = zeros(1, nqp);
+    for ii = 1 : 3
+        for jj = 1 : 3
+            wq(1, jj) = 0.109951743655322;
+            if ii == jj
+                triangular_qp(ii, jj) = 0.816847572980459;
+            else
+                triangular_qp(ii, jj) = 0.091576213509771;
+            end
+        end
+        for jj = 4 : 6
+            wq(1, jj) = 0.223381589678011;
+            if ii == jj - 3
+                triangular_qp(ii, jj) = 0.108103018168070;
+            else
+                triangular_qp(ii, jj) = 0.445948490915965;
+            end
+        end
+    end
+    
+elseif degree == 5
+    nqp = 7;
+    triangular_qp = zeros(3, nqp);
+    wq = zeros(1, nqp);
+    wq(1, 1) = 0.22500;
+    for ii = 1 : 3
+        triangular_qp(ii, 1) = 0.333333333333333;
+        for jj = 2 : 4
+            if ii == jj - 1
+                triangular_qp(ii, jj) = 0.797426985353087;
+            else
+                triangular_qp(ii, jj) = 0.101286507323456;
+            end
+            wq(1, jj) = 0.125939180544827;
+        end
+        for jj = 5 : 7
+            if ii == jj - 4
+                triangular_qp(ii, jj) = 0.059715871789770;
+            else
+                triangular_qp(ii, jj) = 0.470142064105115;
+            end
+            wq(1, jj) = 0.132394152788506;
+        end
+    end
+    
+elseif degree == 6
+    nqp = 12;
+    triangular_qp = zeros(3, nqp);
+    wq = zeros(1, nqp);
+    for ii = 1 : 3
+        for jj = 1 : 3
+            wq(1, jj) = 0.050844906370207;
+            if ii == jj
+                triangular_qp(ii, jj) = 0.873821971016996;
+            else
+                triangular_qp(ii, jj) = 0.063089014491502;
+            end
+        end
+        for jj = 4 : 6
+            wq(1, jj) = 0.116786275726379;
+            if ii == jj - 3
+                triangular_qp(ii, jj) = 0.501426509658179;
+            else
+                triangular_qp(ii, jj) = 0.249286745170910;
+            end
+        end
+        for jj = 7 : 12
+            wq(1, jj) = 0.082851075618374;
+        end
+        r = 0.636502499121399;
+        s = 0.310352451033785;
+        t = 0.053145049844816;
+        triangular_qp(:, 7) = [r, s, t]';
+        triangular_qp(:, 8) = [s, t, r]';
+        triangular_qp(:, 9) = [t, r, s]';
+        triangular_qp(:, 10) = [r, t, s]';
+        triangular_qp(:, 11) = [t, s, r]';
+        triangular_qp(:, 12) = [s, r, t]';
+    end
+    
+elseif degree == 7
+    nqp = 13;
+    triangular_qp = zeros(3, nqp);
+    wq = zeros(1, nqp);
+    wq(1, 1) = -0.149570044467670;
+    for ii = 1 : 3
+        triangular_qp(ii, 1) = 0.333333333333333;
+        for jj = 2 : 4
+            wq(1, jj) = 0.175615257433204;
+            if ii == jj - 1
+                triangular_qp(ii, jj) = 0.479308067841923;
+            else
+                triangular_qp(ii, jj) = 0.260345966079038;
+            end
+        end
+        for jj = 5 : 7
+            wq(1, jj) = 0.053347235608839;
+            if ii == jj - 4
+                triangular_qp(ii, jj) = 0.869739794195568;
+            else
+                triangular_qp(ii, jj) = 0.065130102902216;
+            end
+        end
+        for jj = 8 : 13
+            wq(1, jj) = 0.077113760890257;
+        end
+        r = 0.638444188569809;
+        s = 0.312865496004875;
+        t = 0.086903154253160;
+        triangular_qp(:, 7) = [r, s, t]';
+        triangular_qp(:, 8) = [s, t, r]';
+        triangular_qp(:, 9) = [t, r, s]';
+        triangular_qp(:, 10) = [r, t, s]';
+        triangular_qp(:, 11) = [t, s, r]';
+        triangular_qp(:, 12) = [s, r, t]';
+    end
+    
+else
+        disp('TriangularQuad: Please check the data input.');
+        disp("The degree input should be a interger from 2 to 7.");
+        return;
+end
+
+% Map qp into (xi,eta) space.
+qp = zeros(2, nqp);
+p1 = [-1,-1]';
+p2 = [1, -1]';
+p3 = [0, 1]';
+
+for kk = 1 : nqp
+    qp(1, kk) = p1(1) * triangular_qp(1, kk) + p2(1) * triangular_qp(2, kk)...
+        + p3(1) * triangular_qp(3, kk);
+    qp(2, kk) = p1(2) * triangular_qp(1, kk) + p2(2) * triangular_qp(2, kk)...
+        + p3(2) * triangular_qp(3, kk);
+end
+
+end
+
--- main.m
+++ main.m
@@ -1,10 +1,15 @@
 clc; clear all;
 
+% ---------- Data given ----------
 msh = load_gmsh2('example4-2.msh');
 
 num_Diri = 2;
 num_Neum = 3;
 
+E = 1.0e5;   % Young's modulus.
+nu = 0.3;   % Poisson's ratio.
+
+% ---------- Preprocess ----------
 Diri_Nodes = make_Diri_Nodes(msh, num_Diri);
 
 Plane_IEN = make_Plane_IEN(msh);
@@ -12,9 +17,26 @@ Plane_IEN = make_Plane_IEN(msh);
 Neum_IEN = make_Neum_IEN(msh, num_Diri, num_Neum, Plane_IEN);
 
 ID_array = make_ID_array(msh.nbNod);
+% Input Dirichlet BC
 ID_array = rearrange_ID_array(ID_array, Diri_Nodes{1}, 'x');
 ID_array = rearrange_ID_array(ID_array, Diri_Nodes{2}, 'y');
 
 LM_array = make_LM_array(Plane_IEN, ID_array);
 
 Neum_normalvector = make_normalvector(Neum_IEN, msh.POS);
+
+% ---------- Construct stiffness K ---------- 
+n_eq = ID_array(2, msh.nbNod);
+K = zeros(n_eq, n_eq);
+
+% The physical equations for plane-strain problem: sigma = D * epsilon .
+% sigma = [sigma_xx, sigma_yy, tau_xy]'
+% epsilon = [epsilon_xx, epsilon_yy, gama_xy]'
+% Note: gama_xy = 2 * epsilon_xy
+% epsilon_ij = 0.5 * (ui_xj + uj_xi)
+inv_D = [(1-nu^2)/E,  -nu/(1-nu),   0;
+         -nu/(1-nu),  (1-nu^2)/E,   0;
+         0,            0,    2*(1+nu)/E];
+D = inv(inv_D);
+
+[qp, wq, nqp] = TriangularQuad(7);
